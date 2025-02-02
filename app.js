d65ddea5647011cd1a6d51ef625ff2b94b711b89
Asu1996 <asutoshrt96@gmail.com>
2025-02-02 12:00:27 +0530

    fix: start listening on port only after db connected

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const { connectDB } = require('./db/connectDb');
 const app = express();
 app.use(express.json());
 
-// TODO: start app only after db connected
-connectDB();
+(async () => {
+    await connectDB();
 
-app.listen(webAppPort, () => {
-    console.log(`Server listening on port ${webAppPort}`);
-});
+    app.listen(webAppPort, () => {
+        console.log(`Server listening on port ${webAppPort}`);
+    });
+})();
 
 app.use('/', require('./routes'));
 
